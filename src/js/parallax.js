a7eb6ab83763fc34d6f6923220c65b07169fa330
Adrien Gasquet <adrien.gsqt@gmail.com>
2024-05-01 12:09:21 +0200

    V1 Release

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,20 +1,57 @@
 var body = document.body,
-    html = document.documentElement;
+  html = document.documentElement;
 
-var height = Math.max( body.scrollHeight, body.offsetHeight, 
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+var height = Math.max(
+  body.scrollHeight,
+  body.offsetHeight,
+  html.clientHeight,
+  html.scrollHeight,
+  html.offsetHeight
+);
 
-window.addEventListener("scroll", function(event){
+var width = Math.max(
+  body.scrollWidth,
+  body.offsetWidth,
+  html.clientWidth,
+  html.scrollWidth,
+  html.offsetWidth
+);
 
-		var fromBottom = height - this.pageYOffset - window.innerHeight;
+var layers = document.getElementsByClassName("parallax");
+var xPos = width/2;
+var yPos = 0;
+var fromCenter = 0;
+var fromBottom = 0;
 
-		var layers = document.getElementsByClassName("parallax");
-		var layer, speed, yPos;
-		for (var i = 0; i < layers.length; i++) {
-			layer = layers[i];
-			speed = layer.getAttribute('data-distance');
-			var yPos = -(fromBottom * speed / 100);
-			layer.setAttribute('style', 'transform: translate3d(0px, ' + yPos + 'px, 0px)');
+function parallax(e){
+  fromBottom = height - this.pageYOffset - window.innerHeight;
+  var layer, speed;
 
-		}
-	});
+  for (var i = 0; i < layers.length; i++) {
+    layer = layers[i];
+    speed = layer.getAttribute("data-distance");
+
+    xPos = -(((fromCenter - (width/2)) * (0 - speed)) / 3500);
+    yPos = -((fromBottom * speed) / 100);
+
+    layer.setAttribute(
+      "style",
+      "transform: translate(" + xPos + "px, " + yPos + "px)",
+    );
+    layer.style.zIndex = layer.getAttribute('data-distance')*-1
+  }
+}
+
+
+window.addEventListener("scroll", (e) => {
+  if(fromBottom < 10000){
+    parallax(e)
+  }
+});
+
+window.addEventListener('mousemove', (e) => {
+  fromCenter = e.clientX;
+  if(fromBottom < 10000){
+    parallax(e)
+  }
+})
